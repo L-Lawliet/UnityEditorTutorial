7a0e234a1379a5a9a2f994a4ebd81022bc7b5b8a
Lawliet-Company <1577075604@qq.com>
2018-06-08 16:34:38 +0800

    1.转换meta类型为文本，方便以后操作 2.增加AssetPathProperty扩展。

---
+++ AssetPathProperty/AssetPathAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+///
+/// name:AssetPath
+/// author:Administrator
+/// date:2018/1/20 15:19:55
+/// versions:
+/// introduce:
+/// note:
+/// 
+/// </summary>
+namespace Waiting.UnityEngineExtend
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class AssetPathAttribute : PropertyAttribute 
+    {
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public System.Type type { get; set; }
+
+        public AssetPathAttribute(System.Type type)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ AssetPathProperty/Editor/AssetPathPropertyDrawer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Waiting.UnityEngineExtend;
+
+/// <summary>
+///
+/// name:AssetPathPropertyDrawer
+/// author:Administrator
+/// date:2018/1/20 15:23:02
+/// versions:
+/// introduce:
+/// note:
+/// 
+/// </summary>
+namespace Waiting.UnityEditorExtend
+{
+    [CustomPropertyDrawer(typeof(AssetPathAttribute))]
+    public class AssetPathPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            AssetPathAttribute attribute = (AssetPathAttribute)base.attribute;
+
+            if (!attribute.type.IsSubclassOf(typeof(Object)))
+            {
+                //显示文件类型错误提示
+                EditorGUI.HelpBox(position, string.Format("{0} : AssetPathAttribute.type is not subclass of UnityEngine.GameObject", property.displayName), MessageType.Error);
+            }
+            else
+            {
+                if (property.propertyType == SerializedPropertyType.String)
+                {
+                    //绘制属性框
+                    property.stringValue = FileField(position, property.displayName, property.stringValue, attribute.type);
+                }
+                else
+                {
+                    //显示非字符串错误提示
+                    EditorGUI.HelpBox(position, string.Format("{0} : property type is not System.String", property.displayName), MessageType.Error);
+                }
+            }
+        }
+
+        private string FileField(Rect position, string label, string path, System.Type type)
+        {
+            Object file = AssetDatabase.LoadAssetAtPath(path, type);
+
+            EditorGUI.BeginChangeCheck();
+            {
+                file = EditorGUI.ObjectField(position, label, file, type, false);
+            }
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                path = AssetDatabase.GetAssetPath(file);
+            }
+
+            return path;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            AssetPathAttribute attribute = (AssetPathAttribute)base.attribute;
+
+            float baseHeight = base.GetPropertyHeight(property, label);
+
+            if (!attribute.type.IsSubclassOf(typeof(Object)))
+            {
+                return baseHeight + 20.0f;
+            }
+            else if (property.propertyType == SerializedPropertyType.String)
+            {
+                return baseHeight;
+            }
+            else
+            {
+                return baseHeight + 20.0f;
+            }
+        }
+    }
+}
